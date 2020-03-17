7d7487f920825152761f5fced0ab150b8b026aa2
Yazan Arafeh <yazanarafeh24@gmail.com>
2020-03-16 20:35:07 -0700

    PostCell complete

---
+++ Parstagram/PostCell.swift
@@ -0,0 +1,28 @@
+//
+//  PostCell.swift
+//  Parstagram
+//
+//  Created by Yazan Arafeh on 3/15/20.
+//  Copyright © 2020 Arafeh. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
